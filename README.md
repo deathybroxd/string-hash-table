27261600c31144a632a6d6444f4a8501a0a5bb8f
deathybroxd <vincentboogie100@gmail.com>
2026-06-21 04:25:10 -0400

    fixing the revert

--- README.md
+++
@@ -1,2 +0,0 @@
-This is a templated hashtable of key-value pairs implementation in c++
-- main.cpp is a test file.
--- StringHashTable.h
+++ StringHashTable.h
@@ -1,8 +1,8 @@
-/* HashTable .h
+/* StringHashTable .h
 - templated, all implementations here
 */
-#ifndef HASHTABLE_H
-#define HASHTABLE_H
+#ifndef STRINGHASHTABLE_H
+#define STRINGHASHTABLE_H
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,26 +12,26 @@
 #include <functional>
 #include <cstddef>
 
-template <class K, class V>
-class HashTable {
+template <class V>
+class StringHashTable {
 public:
     // constructor
-    HashTable();
+    StringHashTable();
 
     // destructor (default) - no heap allocated memory
-    ~HashTable() = default;
+    ~StringHashTable() = default;
 
     // insert kv
-    void Insert(const K& key, const V& value);
+    void Insert(const std::string& key, const V& value);
 
     // resize hashtable
     void Resize(const std::size_t& newSize);
 
     // get index
-    std::optional<V> Get(const K& key);
+    std::optional<V> Get(const std::string& key);
 
     // remove index
-    std::optional<V> Remove(const K& key);
+    std::optional<V> Remove(const std::string& key);
 
     // size
     std::size_t Size();
@@ -41,22 +41,27 @@ public:
 
 private:
     // index helper function - returns a size_t index
-    std::size_t GetIndex(const K& key, std::size_t tableSize);
+    std::size_t GetIndex(const std::string& key, std::size_t tableSize);
 
-    std::vector<std::list<std::pair<K, V>>> m_table; // the actual hash table, a vector of "buckets" of kv pairs
+    // hasher helper function - returns a size_t hash
+    std::size_t StringHash(const std::string& key);
+
+    std::vector<std::list<std::pair<std::string, V>>> m_table; // the actual hash table, a vector of "buckets" of kv pairs
     std::size_t m_size; 
     static constexpr double LOAD_FACTOR_THRESHOLD = 0.7; // sweetspot value for resizing hash table, 1 waits too long, 0.3 resizes too much
     static constexpr int DEFAULT_TABLE_SIZE = 8;
+    static constexpr std::size_t PRIME_MULTIPLIER = 31; // for StringHash()
+    static constexpr std::size_t LARGE_PRIME_MODULO = 1'000'000'009; // for StringHash()
 };
 
 // default constructor
-template <class K, class V>
-HashTable<K, V>::HashTable() : m_table(DEFAULT_TABLE_SIZE), m_size(0) {}
+template<class V>
+StringHashTable<V>::StringHashTable() : m_table(DEFAULT_TABLE_SIZE), m_size(0) {}
 
-template <class K, class V>
-void HashTable<K, V>::Insert(const K& key, const V& value) {
+template<class V>
+void StringHashTable<V>::Insert(const std::string& key, const V& value) {
     std::size_t index = GetIndex(key, m_table.size());
-    std::list<std::pair<K, V>>& bucket = m_table.at(index);
+    std::list<std::pair<std::string, V>>& bucket = m_table.at(index);
 
     // search for the key, if found just update the corresponding value
     for(auto it = bucket.begin(); it != bucket.end(); ++it) {
@@ -76,14 +81,14 @@ void HashTable<K, V>::Insert(const K& key, const V& value) {
     }
 }
 
-template <class K, class V>
-void HashTable<K, V>::Resize(const std::size_t& newSize) {
-    std::vector<std::list<std::pair<K, V>>> resizedTable(newSize);
+template<class V>
+void StringHashTable<V>::Resize(const std::size_t& newSize) {
+    std::vector<std::list<std::pair<std::string, V>>> resizedTable(newSize);
 
     for(std::size_t i = 0; i < m_table.size(); ++i) {
-        std::list<std::pair<K, V>>& bucket = m_table.at(i);
+        std::list<std::pair<std::string, V>>& bucket = m_table.at(i);
         for(auto it = bucket.begin(); it != bucket.end(); ++it) {
-            K key = it->first;
+            std::string key = it->first;
             V value = it->second;
             std::size_t index = GetIndex(key, newSize);
             resizedTable.at(index).push_back(std::make_pair(key, value));
@@ -92,10 +97,10 @@ void HashTable<K, V>::Resize(const std::size_t& newSize) {
    m_table = std::move(resizedTable); // m_table = resizedTable is fine but move makes it faster
 }
 
-template <class K, class V>
-std::optional<V> HashTable<K, V>::Get(const K& key) {
+template<class V>
+std::optional<V> StringHashTable<V>::Get(const std::string& key) {
     std::size_t index = GetIndex(key, m_table.size());
-    std::list<std::pair<K, V>>& bucket = m_table.at(index);
+    std::list<std::pair<std::string, V>>& bucket = m_table.at(index);
     for(auto it = bucket.begin(); it != bucket.end(); ++it) {
         if(it->first == key) {
             return it->second; // value
@@ -104,10 +109,10 @@ std::optional<V> HashTable<K, V>::Get(const K& key) {
     return std::nullopt;
 }
 
-template <class K, class V>
-std::optional<V> HashTable<K, V>::Remove(const K& key) {
+template<class V>
+std::optional<V> StringHashTable<V>::Remove(const std::string& key) {
     std::size_t index = GetIndex(key, m_table.size());
-    std::list<std::pair<K, V>>& bucket = m_table.at(index);
+    std::list<std::pair<std::string, V>>& bucket = m_table.at(index);
     for(auto it = bucket.begin(); it != bucket.end(); ++it) {
         if(it->first == key) {
             V value = it->second;
@@ -119,13 +124,13 @@ std::optional<V> HashTable<K, V>::Remove(const K& key) {
     return std::nullopt;
 }
 
-template <class K, class V>
-std::size_t HashTable<K, V>::Size() {
+template<class V>
+std::size_t StringHashTable<V>::Size() {
     return m_size;
 }
 
-template <class K, class V>
-bool HashTable<K, V>::IsEmpty() {
+template<class V>
+bool StringHashTable<V>::IsEmpty() {
     if(m_size <= 0) {
         return true;
     } else {
@@ -135,11 +140,19 @@ bool HashTable<K, V>::IsEmpty() {
 
 
 // ***** PRIVATE METHODS *****
-template <class K, class V>
-std::size_t HashTable<K, V>::GetIndex(const K& key, std::size_t tableSize) {
-    std::hash<K> hasher;
-    std::size_t hashValue = hasher(key);
+template<class V>
+std::size_t StringHashTable<V>::GetIndex(const std::string& key, std::size_t tableSize) {
+    std::size_t hashValue = StringHash(key);
     return hashValue % tableSize;
+}
+
+template<class V>
+std::size_t StringHashTable<V>::StringHash(const std::string& key) {
+    std::size_t hash = 0;
+    for(const char& ch : key) {
+        hash = (hash * PRIME_MULTIPLIER + ch) % LARGE_PRIME_MODULO;
+    }
+    return hash;
 }
 
 #endif
